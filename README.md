965b71218edaea5a6a98e1670a15bb65a21f63e0
Tom Fischer <tom.a.fischer@gmail.com>
2018-06-14 12:55:35 -0400

    started ANN

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ai_self_driving_car
+
+## Ubuntu torch install without GPU
+The current solution only works with torch 3.1:
+```
+$ pip install http://download.pytorch.org/whl/cpu/torch-0.3.1-cp36-cp36m-linux_x86_64.whl --cert <path to cert.pem> 
+```
+
+## General
+
+### Git cert issue:
+```
+$ git config --global http.sslVerify false
+```
--- ai.py
+++ ai.py
@@ -11,3 +11,15 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torch.autograd as autograd
 from torch.autograd import Variable
+
+
+#Create the architecture of the Neural Network
+
+class Network(nn.Module):
+    
+    def __init__(self, input_size, nb_action):
+        super(Network,self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 30)
+        
